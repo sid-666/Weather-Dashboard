488be7beb1ad8fd803606e1ef2c4e29065736343
Seance <siddharth.dhananjay@gmail.com>
2020-09-17 08:31:36 +1000

    finished display function on click event, and now going to move onto creating new search history button

--- script.js
+++ script.js
@@ -5,12 +5,12 @@
     var newSPI = "6261fa55f519561be7caea838a0ef085"
     var cityname;
     // Here we are building the URL we need to query the database
-    var today = new Date();
-    var dd = today.getDate();
+    var date = new Date();
+    var dd = date.getDate();
     
 
-    var mm = today.getMonth()+1; 
-    var yyyy = today.getFullYear();
+    var mm = date.getMonth()+1; 
+    var yyyy = date.getFullYear();
     if(dd<10) 
     {
     dd='0'+dd;
@@ -22,10 +22,20 @@
     } 
     today = mm+'/'+dd+'/'+yyyy;
     
+    function createbutton(a){
+        var history = JSON.parse(localStorage.getItem("cities")) || []
+        history.push(a)
+        localStorage.setItem("cities", JSON.stringify(history))
+        var histbtn = $("<button>")
+        histbtn.text(history[history.length - 1]).attr("class", "historybtn")
+        $("#sbtnarea").prepend(histbtn)
+    }
 
     $("#searchbutton").click(function(){
         cityname = $(".input").val().trim()
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityname+"&appid="+newSPI
+        // Creating new button
+        createbutton(cityname)
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -43,11 +53,20 @@
                  $("#wicon").attr("src", iconurl)
                 currentweather.find("#cheader").text(`${cityname} ${today}`)
                 var temperature = (response.daily[0].temp.day - 273.15) * 1.80 + 32;
-                currentweather.find("#temp").text("Temperature:" + " "+ temperature + "F")
+                currentweather.find("#temp").text("Temperature:" + " "+ temperature.toFixed(2) + "F")
+                currentweather.find("#humid").text("Humidity:" + " " + response.daily[0].humidity)
+                currentweather.find("#wspeed").text("Wind Speed:"+" "+response.daily[0].wind_speed+"km/h")
+                currentweather.find("#uv").text("UV Index:"+" "+response.daily[0].uvi)
+                // Weeks Forecast
+                $(".WF").each(function(index,item){
+                    dd++;
+                    console.log(dd)
+                    $(item).find("#ndate").text(today)
+                    $(item).find("#wicon").attr("src", "http://openweathermap.org/img/w/" + response.daily[index+1].weather[0].icon + ".png" )
+                    $(item).find("#tem").text("Temp:"+" "+((response.daily[index+1].temp.day - 273.15) * 1.80 + 32).toFixed(2)+ "F")
+                    $(item).find("#hum").text("Humidity:" + " " + response.daily[index+1].humidity)
+                })
                 // only save the valid city names
-                // var history = JSON.parse(localStorage.getItem("cities")) || []
-                // history.push(cityname)
-                // localStorage.setItem("cities", JSON.stringify(history))
             })
         })
 
