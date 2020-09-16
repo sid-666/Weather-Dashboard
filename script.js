53a3b3816ac682bddaf67e02756aaee5d66f6599
Seance <siddharth.dhananjay@gmail.com>
2020-09-16 16:13:13 +1000

    started developing the click event and testing the response from API

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
     // This is our API key. Add your own API key between the ""
-    var APIKey = "6261fa55f519561be7caea838a0ef085";
-    var cityname = $("input").val().trim()
-
+    var APIKey = "e5d3d883d923c69ea77aa69a9e9734ab";
+    var newSPI = "6261fa55f519561be7caea838a0ef085"
+    var cityname;
     // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=" + APIKey;
+    var today = new Date();
+    var dd = today.getDate();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-  
-        // Create CODE HERE to Log the queryURL
-        console.log(queryURL)
-        // Create CODE HERE to log the resulting object
-        console.log(response)
-        // Create CODE HERE to calculate the temperature (converted from Kelvin)
-  
-        // Create CODE HERE to transfer content to HTML
-        // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-        // Create CODE HERE to dump the temperature content into HTML
-  
-      });
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+    if(dd<10) 
+    {
+    dd='0'+dd;
+    } 
+
+    if(mm<10) 
+    {
+        mm='0'+mm;
+    } 
+    today = mm+'/'+dd+'/'+yyyy;
+    
+
+    $("#searchbutton").click(function(){
+        localStorage.setItem("cityname",$(".input").val().trim());
+        cityname = localStorage.getItem("cityname")
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityname+"&appid="+newSPI
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+              // Create CODE HERE to log the resulting object
+            console.log(response)
+            var latitude = response.coord.lat
+            var longitude = response.coord.lon
+            return $.ajax({
+                url: "https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&exclude=minutely,hourlydaily&appid="+ newSPI
+            }).then(function(response){
+                console.log(response)
+            })
+        })
+
+    })
